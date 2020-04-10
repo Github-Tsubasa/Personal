09027d2319497c8fee2dc2adbca9fd6662cfcb10
Github-Tsubasa <59303334+Github-Tsubasa@users.noreply.github.com>
2020-04-10 22:54:12 +0900

    ユーザー情報編集機能の追加,ログアウト機能の追加 22:53 April 10

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'posts', to: 'posts#index'
-  
+  root "messages#index"
+  resources :users, only: [:edit, :update]
   
   resources :tweets, only: [:index, :new]
   resources :groups, only: [:new, :create]
